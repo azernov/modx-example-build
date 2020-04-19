6fda265d2e5062040b5487180478a963e25905d0
artprog <aprog-art@mail.ru>
2020-04-19 13:38:52 +0700

    Убраны остатки из старой сборки

--- db/dbdata/modx_namespaces.sql
+++ db/dbdata/modx_namespaces.sql
@@ -11,14 +11,12 @@
 /*!40000 ALTER TABLE `modx_namespaces` DISABLE KEYS */;
 REPLACE INTO `modx_namespaces` VALUES ('core','{core_path}','{assets_path}');
 REPLACE INTO `modx_namespaces` VALUES ('formit','{core_path}components/formit/','{assets_path}components/formit/');
-REPLACE INTO `modx_namespaces` VALUES ('mspsberbank','{core_path}components/mspsberbank/','');
 REPLACE INTO `modx_namespaces` VALUES ('tinymce','{core_path}components/tinymce/','{assets_path}components/tinymce/');
 REPLACE INTO `modx_namespaces` VALUES ('translit','{core_path}components/translit/','');
 REPLACE INTO `modx_namespaces` VALUES ('breadcrumb','{core_path}components/breadcrumb/','');
 REPLACE INTO `modx_namespaces` VALUES ('codemirror','{core_path}components/codemirror/','');
 REPLACE INTO `modx_namespaces` VALUES ('pdotools','{core_path}components/pdotools/','');
 REPLACE INTO `modx_namespaces` VALUES ('ajaxform','{core_path}components/ajaxform/','');
-REPLACE INTO `modx_namespaces` VALUES ('ms2gallery','{core_path}components/ms2gallery/','');
 /*!40000 ALTER TABLE `modx_namespaces` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
